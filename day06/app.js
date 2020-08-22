1e690cc0916951c5c428b7751d04effa62d2f29c
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-23 03:00:38 +0900

    Update movie modal

--- day06/app.js
+++ day06/app.js
@@ -1538,6 +1538,9 @@ const genres = {
 const $buttonContainer = document.querySelector('.button-container');
 const $movielist = document.querySelector('.movielist');
 const $message = document.querySelector('.message');
+const $modalOverlay = document.querySelector('.modal-overlay');
+const $modal = document.querySelector('.modal');
+const $modalExitButton = document.querySelector('.modal-exit-button');
 
 function filterMovie(movies, genreIds) {
     let filteredMovies = movies.slice();
@@ -1564,20 +1567,21 @@ function setMessage(movies) {
 function makeMovieBox(movie) {
     const $movie = document.createElement('div');
     $movie.className = 'movie';
+    $movie.dataset.id = movie.id;
     if (movie.poster_path) {
         $movie.style.background = `url(https://image.tmdb.org/t/p/w200${movie.poster_path}) no-repeat 0 0 / cover`;
     } else $movie.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
 
     const $title = document.createElement('span');
-    $title.className = 'content title';
+    $title.className = 'contents title';
     $title.textContent = movie.title;
 
     const $rating = document.createElement('span');
-    $rating.className = 'content rating';
+    $rating.className = 'contents rating';
     $rating.textContent = `⭐${movie.vote_average}`;
 
     const $released = document.createElement('span');
-    $released.className = 'content released';
+    $released.className = 'contents released';
     $released.textContent = movie.release_date.split('-').join('.');
     $movie.append($title, $released, $rating);
 
@@ -1604,17 +1608,60 @@ function handleClickButton(e) {
     }
 }
 
+function makeModal(movie) {
+    const $poster = document.querySelector('.poster');
+    if (movie.poster_path) {
+        $poster.style.background = `url(https://image.tmdb.org/t/p/w200${movie.poster_path}) no-repeat 0 0 / cover`;
+    } else $poster.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+
+    const $title = document.querySelector('.modal-title');
+    $title.textContent =
+        movie.title === movie.original_title
+            ? movie.title
+            : `${movie.title}(${movie.original_title})`;
+
+    const $detail = document.querySelector('.detail');
+    $detail.innerHTML = `${movie.release_date
+        .split('-')
+        .join('.')}<span class="divider">|</span>⭐ ${
+        movie.vote_average
+    }<span class="divider">|</span><span title="vote count">${movie.vote_count}</span>`;
+
+    const $overview = document.querySelector('.overview');
+    $overview.textContent = movie.overview;
+
+    $modal.style.top = `${document.documentElement.scrollTop}px`;
+    document.body.style.overflow = 'hidden';
+    $modalOverlay.classList.remove('hidden');
+    $modal.classList.remove('hidden');
+}
+
 function handleClickMovie(e) {
     const { target } = e;
+    console.log(target);
+    let movieId;
     if (target.classList.contains('movie')) {
-        console.log('movie');
+        movieId = target.dataset.id;
+    } else if (target.classList.contains('contents')) {
+        movieId = target.parentElement.dataset.id;
     }
+    console.log(movieId);
+    const clickedMovie = movies.filter((movie) => movie.id === Number(movieId))[0];
+    makeModal(clickedMovie);
+}
+
+function handleExitModal() {
+    $modalOverlay.classList.add('hidden');
+    $modal.classList.add('hidden');
+    document.body.style.overflow = 'visible';
 }
 
 function init() {
     renderMovies(movies);
     $buttonContainer.addEventListener('click', handleClickButton);
     $movielist.addEventListener('click', handleClickMovie);
+    $modalOverlay.addEventListener('click', handleExitModal);
+    $modalExitButton.addEventListener('click', handleExitModal);
 }
 
 init();
