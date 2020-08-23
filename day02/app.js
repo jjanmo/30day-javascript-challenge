72fd3ff387786bd6115736a766821c6626eadcd0
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-23 13:57:57 +0900

    Update analog clock style

--- day02/app.js
+++ day02/app.js
@@ -13,76 +13,78 @@ let digitalInterval;
 let analogInterval;
 
 function getTime() {
-	const now = new Date();
-	const hour = now.getHours();
-	const minute = now.getMinutes();
-	const second = now.getSeconds();
-	return {
-		hour,
-		minute,
-		second,
-	};
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+    return {
+        hour,
+        minute,
+        second,
+    };
 }
 
 function paintAnalogClock() {
-	const $analogHour = document.querySelector('.analog-hour');
-	const $analogMinute = document.querySelector('.analog-minute');
-	const $analogSecond = document.querySelector('.analog-second');
-	const timeObj = getTime();
+    const $analogHour = document.querySelector('.analog-hour');
+    const $analogMinute = document.querySelector('.analog-minute');
+    const $analogSecond = document.querySelector('.analog-second');
+    const timeObj = getTime();
 
-	$analogHour.style.transform = `rotate(${timeObj.hour * 30}deg)`;
-	$analogMinute.style.transform = `rotate(${timeObj.minute * 6}deg)`;
-	$analogSecond.style.transform = `rotate(${timeObj.second * 6}deg)`;
+    $analogHour.style.transform = `rotate(${timeObj.hour * 30}deg)`;
+    $analogMinute.style.transform = `rotate(${timeObj.minute * 6}deg)`;
+    $analogSecond.style.transform = `rotate(${timeObj.second * 6}deg)`;
 }
 
 function paintDigitalClock() {
-	const $hour = $digitalClock.querySelector('.digital-hour');
-	const $minute = $digitalClock.querySelector('.digital-minute');
-	const $second = $digitalClock.querySelector('.digital-second');
-	const timeObj = getTime();
+    const $hour = $digitalClock.querySelector('.digital-hour');
+    const $minute = $digitalClock.querySelector('.digital-minute');
+    const $second = $digitalClock.querySelector('.digital-second');
+    const timeObj = getTime();
 
-	$hour.textContent = timeObj.hour < 10 ? `0${timeObj.hour}` : timeObj.hour;
-	$minute.textContent = timeObj.minute < 10 ? `0${timeObj.minute}` : timeObj.minute;
-	$second.textContent = timeObj.second < 10 ? `0${timeObj.second}` : timeObj.second;
+    $hour.textContent = timeObj.hour < 10 ? `0${timeObj.hour}` : timeObj.hour;
+    $minute.textContent = timeObj.minute < 10 ? `0${timeObj.minute}` : timeObj.minute;
+    $second.textContent = timeObj.second < 10 ? `0${timeObj.second}` : timeObj.second;
 }
 
 function handleClickButton(e) {
-	const {
-		target: { textContent },
-	} = e;
+    const {
+        target: { textContent },
+    } = e;
 
-	if (textContent === 'digital') {
-		$digitalClockContainer.classList.remove('hidden');
-		$buttonContainer.classList.add('hidden');
-		analogInterval && clearInterval(analogInterval);
-		paintDigitalClock();
-		digitalInterval = setInterval(() => {
-			paintDigitalClock();
-		}, 1000);
-	} else {
-		$analogClockContainer.classList.remove('hidden');
-		$buttonContainer.classList.add('hidden');
-		digitalInterval && clearInterval(digitalInterval);
-		paintAnalogClock();
-		analogInterval = setInterval(() => {
-			paintAnalogClock();
-		}, 1000);
-	}
+    if (textContent === 'digital') {
+        $digitalClockContainer.classList.remove('hidden');
+        $buttonContainer.classList.add('hidden');
+        analogInterval && clearInterval(analogInterval);
+        paintDigitalClock();
+        digitalInterval = setInterval(() => {
+            paintDigitalClock();
+        }, 1000);
+    } else {
+        $analogClockContainer.classList.remove('hidden');
+        $buttonContainer.classList.add('hidden');
+        digitalInterval && clearInterval(digitalInterval);
+        paintAnalogClock();
+        analogInterval = setInterval(() => {
+            paintAnalogClock();
+        }, 1000);
+    }
 }
 
 function handleCloseButton(e) {
-	const {
-		target: { parentElement },
-	} = e;
+    const {
+        target: { parentElement },
+    } = e;
 
-	parentElement.classList.add('hidden');
-	$buttonContainer.classList.remove('hidden');
+    parentElement.classList.add('hidden');
+    $buttonContainer.classList.remove('hidden');
 }
 
 function init() {
-	$analogButton.addEventListener('click', handleClickButton);
-	$digitalButton.addEventListener('click', handleClickButton);
-	$clossButtons.forEach((closeButton) => closeButton.addEventListener('click', handleCloseButton));
+    $analogButton.addEventListener('click', handleClickButton);
+    $digitalButton.addEventListener('click', handleClickButton);
+    $clossButtons.forEach((closeButton) =>
+        closeButton.addEventListener('click', handleCloseButton)
+    );
 }
 
 init();
